7698df29de4c39acb2a41c70b58733ba61b86165
zz <zhuguang@ejoy.com>
2019-09-11 07:33:31 +0800

    [ADDED] WxToolbarPanel

---
+++ include/intention/WxToolbarPanel.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <ee0/Observer.h>
+
+#include <wx/panel.h>
+
+namespace ee0 { class WxStagePage; class WxNavigationBar; }
+
+namespace itt
+{
+
+class WxGeoProperty;
+class WxNodeProperty;
+
+class WxToolbarPanel : public wxPanel, public ee0::Observer
+{
+public:
+	WxToolbarPanel(wxWindow* parent, ee0::WxStagePage* stage_page);
+
+	virtual void OnNotify(uint32_t msg, const ee0::VariantSet& variants) override;
+
+private:
+	void InitLayout();
+
+    void OnSelectionInsert(const ee0::VariantSet& variants);
+    void OnSelectionClear(const ee0::VariantSet& variants);
+
+    void OnRootToNextLevel(const ee0::VariantSet& variants);
+
+private:
+    ee0::WxStagePage* m_stage_page;
+
+    WxGeoProperty* m_geo_prop;
+
+    WxNodeProperty* m_node_prop;
+
+    ee0::WxNavigationBar* m_nav_bar;
+
+}; // WxToolbarPanel
+
+}
