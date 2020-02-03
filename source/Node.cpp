bc6c4a42beadc114f7a60f2fa539bdd836675d23
zz <xzrunner@gmail.com>
2020-02-04 05:51:54 +0800

    rename

--- source/Node.cpp
+++ source/Node.cpp
@@ -186,7 +186,7 @@ void Node::InitPins(const std::string& name)
 
 			auto& s = src[i];
             d.type = SOPAdapter::TypeBackToFront(s.var.type);
-            d.name = s.var.name;
+            d.name = s.var.full_name;
 
             dst.push_back(d);
 		}
